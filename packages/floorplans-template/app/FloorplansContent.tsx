e620b6de6a3c96b2dd0986a8253e5415f4640f02
nasla <nasla@qburst.com>
2026-01-30 12:37:10 +0530

    integrated floor plan html

---
+++ packages/floorplans-template/app/FloorplansContent.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useEffect } from "react";
+
+interface FloorplansContentProps {
+  htmlContent: string;
+  floorplansBaseUrl: string;
+}
+
+export function FloorplansContent({
+  htmlContent,
+  floorplansBaseUrl,
+}: FloorplansContentProps) {
+  useEffect(() => {
+    const stylesheetHref = `${floorplansBaseUrl}/assets/floorplan_modern.css`;
+    const existingStylesheet = document.querySelector(
+      `link[href*="floorplan_modern.css"]`
+    );
+    if (!existingStylesheet) {
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.href = stylesheetHref;
+      document.head.appendChild(link);
+    }
+
+    const scriptSrc = `${floorplansBaseUrl}/assets/script.js`;
+    const existingScript = document.querySelector(
+      'script[src*="assets/script.js"]'
+    );
+    if (!existingScript) {
+      const script = document.createElement("script");
+      script.src = scriptSrc;
+      script.async = true;
+      document.body.appendChild(script);
+    }
+
+    return () => {
+      const scriptToRemove = document.querySelector(
+        'script[src*="assets/script.js"]'
+      );
+      if (scriptToRemove && scriptToRemove !== existingScript) {
+        scriptToRemove.remove();
+      }
+    };
+  }, [floorplansBaseUrl, htmlContent]);
+
+  return (
+    <div
+      id="floorplans-content"
+      className="floorplans-content"
+      dangerouslySetInnerHTML={{ __html: htmlContent }}
+    />
+  );
+}
+
